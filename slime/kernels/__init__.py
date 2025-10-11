4ff8d3d04c9775409ce5a12423094e058ef36ca9
Recursive Gardener <garden@recursive.dev>
2025-10-11 16:55:58 -0700

    Update kernels init exports

--- slime/kernels/__init__.py
+++ slime/kernels/__init__.py
@@ -1,6 +1,6 @@
 """GPU kernels for slime mold routing"""
 
-from slime.kernels.routing import fused_routing_kernel
 from slime.kernels.utils import safe_grid_config, validate_tensor
+from slime.kernels.torch_fallback import TorchKernel
 
-__all__ = ["fused_routing_kernel", "safe_grid_config", "validate_tensor"]
+__all__ = ["safe_grid_config", "validate_tensor", "TorchKernel"]
