bad19ddd245469dcb63afa2c0abb8499ffe3b25e
Recursive Gardener <garden@recursive.dev>
2025-10-11 16:54:18 -0700

    Add TubeNetwork implementing Memory protocol

--- slime/memory/__init__.py
+++ slime/memory/__init__.py
@@ -2,10 +2,12 @@
 
 from slime.memory.archive import BehavioralArchive, Elite
 from slime.memory.pool import DynamicPool, PoolConfig
+from slime.memory.tubes import TubeNetwork
 
 __all__ = [
     "BehavioralArchive",
     "Elite",
     "DynamicPool",
     "PoolConfig",
+    "TubeNetwork",
 ]
