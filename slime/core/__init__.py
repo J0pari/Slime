ba5402cc08d553175ab782a7f4c517597f39635f
Recursive Gardener <garden@recursive.dev>
2025-10-11 17:04:20 -0700

    Export Chemotaxis from core

--- slime/core/__init__.py
+++ slime/core/__init__.py
@@ -3,5 +3,6 @@
 from slime.core.pseudopod import Pseudopod
 from slime.core.organism import Organism
 from slime.core.state import FlowState
+from slime.core.chemotaxis import Chemotaxis
 
-__all__ = ["Pseudopod", "Organism", "FlowState"]
+__all__ = ["Pseudopod", "Organism", "FlowState", "Chemotaxis"]
